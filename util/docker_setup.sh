4652838edfa426be8b6447c00a38e0c2b8376769
milanariga <40401188+milanariga@users.noreply.github.com>
2023-01-10 22:06:13 +0200

    Update docker_setup.sh

--- util/docker_setup.sh
+++ util/docker_setup.sh
@@ -16,6 +16,6 @@ cp ngnix-configuration.conf /etc/nginx/sites-available/2050.conf
 ln -sf /etc/nginx/sites-available/2050.conf /etc/nginx/sites-enabled/2050.conf
 unlink /etc/nginx/sites-enabled/default
 nginx -t
-service nginx restart
+#service nginx restart
 
 echo "The code should now be available on this computer"
